ea084863ebf438e23bd08da17ef5d55595aad55a
dani-mcfarlin-group <danielle@mcfarlin-group.com>
2024-12-29 17:48:17 +0800

    app.py

--- app.py
+++ app.py
@@ -21,5 +21,3 @@ if st.button("Submit"):
 	            )
            st.success("Response:")
            st.write(response.choices[0].text.strip())
-       #except Exception as e:
-           #st.error("An error occurred. Please check your input or try again.")
